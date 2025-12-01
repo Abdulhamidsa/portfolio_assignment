7ac5cc47959eff8f12f9a0b53c984f9a6af8a4cc
ziakhan82 <ziaullahhassan8@gmail.com>
2025-12-01 16:42:45 +0100

    adding people hook to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import useGetMovies from "./hooks/useGetMovies";
+import useGetPeople from "./hooks/useGetPeople";
 
 function App() {
   const { movies } = useGetMovies();
   console.log(movies);
+
+  const { people } = useGetPeople();
+  console.log(people);
+
 
   return <></>;
 }
