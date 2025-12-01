f26781ea965a0098057b2eb8108d19fbcd42a5e7
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-12-01 11:33:56 +0100

    setting up a hook to get all movies + add a util helper for endpoints

---
+++ src/hooks/useGetMovies.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { ENDPOINTS } from "../util/endpoints";
+const useGetMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(ENDPOINTS.GET_ALL_TITLES);
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch movies");
+        }
+
+        const data = await response.json();
+        setMovies(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return { movies, loading, error };
+};
+
+export default useGetMovies;
