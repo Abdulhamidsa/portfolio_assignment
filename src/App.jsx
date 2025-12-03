9785cfcef00eeeda7ce1853a92e1dafd72b86ab6
Rumana-Oops <rumana.afrin1995@gmail.com>
2025-12-03 14:59:48 +0100

    LogIn Componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
-import { Button } from "react-bootstrap";
-import Accordion from "react-bootstrap/Accordion";
-import BasicExample from "./components/BasicExample.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn.jsx";
+import SignUp from "./components/SignUp.jsx";
+import SignOut from "./components/SignOut.jsx";
+import Profile from "./components/Profile.jsx";
+import AppNavbar from "./components/AppNavbar.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 function App() {
   return (
-    <>
-      <BasicExample />
-    </>
+    <AuthProvider>
+      <Router>
+        <AppNavbar />
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signout" element={<SignOut />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
