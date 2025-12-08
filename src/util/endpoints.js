58cc3fe21500df7a23b56933876385fe82a14071
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-12-08 13:17:50 +0100

    fix: remove duplicate GET_PERSON_BY_ID endpoint from endpoints

--- src/util/endpoints.js
+++ src/util/endpoints.js
@@ -13,11 +13,7 @@ export const ENDPOINTS = {
     GET_TITLE_BY_ID: (id) => `${API_BASE_URL}/Titles/${id}`,
     SEARCH_TITLES: (query) => `${API_BASE_URL}/Titles/search?query=${encodeURIComponent(query)}`,
     GET_POPULAR_PEOPLE: `${API_BASE_URL}/People/popular`,
-<<<<<<< HEAD
-    GET_PERSON_BY_ID: (nconst) => `${API_BASE_URL}/People/${nconst}`,
-=======
     GET_BOOKMARKS: `${API_BASE_URL}/Bookmarks`,
     GET_CURRENT_USER: `${API_BASE_URL}/Auth/me`,
->>>>>>> a0d306ec3baeeb1cce7266bdbfbbb8d10db0bd47
   },
 };
