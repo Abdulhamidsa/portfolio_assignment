03166bbcc1448c4cc836666b837e1f7e0fc4ddbc
ziakhan82 <ziaullahhassan8@gmail.com>
2025-12-15 14:41:16 +0100

    fetch people data using useGetPeople hook

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -5,7 +5,11 @@ import "./people.css";
 const ITEMS_PER_PAGE = 6;
 
 const People = () => {
+  const scrollRef = useRef(null);
+  const [page, setPage] = useState(0);
+  const [cardWidth, setCardWidth] = useState(0);
+
+  const { people, loading, error } = useGetPeople();
+
   return null;
 };
-
-export default People;
