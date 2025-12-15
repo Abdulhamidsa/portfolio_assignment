b3bacb6d4fe176d18df89c102e450605b569c82b
ziakhan82 <ziaullahhassan8@gmail.com>
2025-12-15 14:33:36 +0100

    created People component with base setup

---
+++ src/components/People/People.jsx
@@ -0,0 +1,11 @@
+import { useRef, useState, useEffect } from "react";
+import useGetPeople from "../../hooks/useGetPeople";
+import "./people.css";
+
+const ITEMS_PER_PAGE = 6;
+
+const People = () => {
+  return null;
+};
+
+export default People;
